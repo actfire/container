87b3dfd10f4306f92db67d0045de73d3f4a21124
Andersen <actfire@gmail.com>
2022-05-25 20:18:44 +0800

    更新腳本 & 測試ok

--- vagrant/Centos8_Jenkins/run_script.sh
+++ vagrant/Centos8_Jenkins/run_script.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# 安裝 nginx
+yum install epel-release -y
+yum install nginx -y
+systemctl enable nginx
+nginx
+
 # 安裝 jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum upgrade
-yum install epel-release java-11-openjdk-devel -y
+yum install fontconfig epel-release java-11-openjdk -y
 yum install jenkins -y
-systemctl daemon-reload
 
+systemctl daemon-reload
 systemctl start jenkins
 systemctl enable jenkins
